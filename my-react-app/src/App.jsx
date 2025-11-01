1b9b4b45df4a2c5483c007ded7c233b6b9412dd3
Rain0829 <rain08290706@gmail.com>
2025-11-01 17:25:56 -0400

    Searching and course content

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,19 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
-
+import "./App.css";
 import Header from "./Header.jsx";
+import Searching from "./Searching.jsx";
+import Content from "./Content.jsx";
 import Footer from "./Footer.jsx";
 
 function App() {
-
     return (
-        <>
+        <div className="app-shell">
             <Header />
-            <Footer />
-        </>
+            <Searching />
+
+            {/* this is the part that must grow */}
+            <main className="page-content">
+                <Content />
+
+                <Footer />
+
+            </main>
+
+        </div>
     );
 }
 
-export default App
+export default App;
