d8cfb2c32c3243c03281e76665ed3a54569feba6
Rain0829 <rain08290706@gmail.com>
2025-11-01 14:40:10 -0400

    basic stuff

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+
+import Header from "./Header.jsx";
+import Footer from "./Footer.jsx";
+
+function App() {
+
+    return (
+        <>
+            <Header />
+            <Footer />
+        </>
+    );
+}
+
+export default App
---
+++ my-react-app/src/Header.jsx
@@ -0,0 +1,19 @@
+function Header(){
+
+    return (
+        <header className="CouseerHub" >
+            <h1>
+                CouseerHub
+            </h1>
+            <nav className="nav">
+                <ul>
+                    <li><a></a>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
