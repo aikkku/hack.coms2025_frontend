65c6ed6e6278908af72607d7946912990a4baa30
Rain0829 <rain08290706@gmail.com>
2025-11-01 16:29:16 -0400

    Header and footer

--- my-react-app/src/Footer.jsx
+++ my-react-app/src/Footer.jsx
@@ -3,14 +3,26 @@ import "./Footer.css";
 function Footer() {
     return (
         <footer className="footer">
-            <div className="footer-left">
-                <h3>URSmart</h3>
-                <p>© 2025 University of Rochester. All rights reserved.</p>
+            <div className="footer-title">URSmart</div>
+
+            <div className="footer-center">
+                <div className="footer-section">
+                    <h4>Contact</h4>
+                    <a href="mxiia8@u.rochester.edu">mxiia8@u.rochester.edu</a>
+                </div>
+
+                <div className="footer-section">
+                    <h4>People</h4>
+                    <a href="#rain">Rain Xia</a>
+                    <a href="#earvin">Earvin Chen</a>
+                    <a href="#asilbek ">Asilbek Ismatilloev</a>
+                    <a href="#david ">David Yen</a>
+
+                </div>
             </div>
 
-            <div className="footer-right">
-                <a href="#contact">Contact</a>
-                <a href="#people">People</a>
+            <div className="footer-bottom">
+                © 2025 University of Rochester. All rights reserved.
             </div>
         </footer>
     );
