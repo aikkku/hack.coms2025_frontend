09f24b6c9f97542e14a6983852c03bed862690db
earvin4444 <earvin4444@gmail.com>
2025-11-02 01:41:06 -0400

    design updates

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './Header.css'
 import App from './App.jsx'
+import './globals.css';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
